e9ed994f68a26a7db2977275d14273a61143e5cd
Jayden Tavares <jayden.tavares@hotmail.com>
2022-05-20 19:08:24 +1000

    add character turn signals to idle/active states via code

--- src/character/states/Active.gd
+++ src/character/states/Active.gd
@@ -2,5 +2,9 @@ class_name Active
 extends State
 
 
+func _ready() -> void:
+    owner.connect("turn_ended", self, "_on_Character_turn_ended")
+
+
 func _on_Character_turn_ended(character: Character) -> void:
     state_machine.transition_to_state("Idle")
--- src/character/states/Idle.gd
+++ src/character/states/Idle.gd
@@ -1,5 +1,9 @@
 extends State
 
 
+func _ready() -> void:
+    owner.connect("turn_started", self, "_on_Character_turn_started")
+
+
 func _on_Character_turn_started(character: Character) -> void:
     state_machine.transition_to_state("Active")
