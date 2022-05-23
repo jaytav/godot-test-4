2cdb81638d7f90761cdcd637fda24b4df0627196
Jayden Tavares <jayden.tavares@hotmail.com>
2022-05-23 21:19:53 +1000

    stop health point breath animation on action change

--- src/character/gui/HealthPoint.gd
+++ src/character/gui/HealthPoint.gd
@@ -10,6 +10,15 @@ func breath() -> void:
     animation_player.play("Breath")
 
 
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_pressed("ui_focus_next"):
+        if animation_player.is_playing():
+            animation_player.stop()
+
+            if get_parent():
+                modulate = get_parent().health_point_modulate
+
+
 func _on_Mouse_Controller_cell_clicked(cell: Vector2) -> void:
     MouseController.disconnect("cell_clicked", self, "_on_Mouse_Controller_cell_clicked")
     MouseController.disconnect("cell_updated", self, "_on_Mouse_Controller_cell_updated")
